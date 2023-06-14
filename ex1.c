e32700bc9e41ac1cd331a5dda1b8aabc93d124ab
s1300220 <136553121+s1300220@users.noreply.github.com>
2023-06-14 14:07:33 +0900

    Dice game: initial revision

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+  int num[2];
+  int sum=0;
+  for(int i=0;i<2;i++){
+    num[i]=rand()%6+1;
+    printf("Die %d : %d\n",i,num[i]);
+    sum+=num[i];
+  }
+  printf("Total value: %d\n" ,sum);
+}
