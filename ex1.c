ef3314da1afd6610b9ba7694005ebd0595f3ce67
s1300220 <136553121+s1300220@users.noreply.github.com>
2023-06-14 14:16:26 +0900

    Greetings added

--- ex1.c
+++ ex1.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
+  char str[9];
+  printf("What is your name?\n");
+  scanf(%s,str);
+  printf("Hello, %s\n",str);
   int num[2];
   int sum=0;
   for(int i=0;i<2;i++){
